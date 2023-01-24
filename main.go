9cc9419ee5d8256761f297715ef47cee3883751b
flyingfrisbee <giovannhiu@gmail.com>
2023-01-24 20:45:09 +0700

    path query params

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
-	"exercisee/db"
+	"fmt"
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	err := db.InitDB()
+	// err := db.InitDB()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	return
+	// }
+
+	// defer db.CloseConnection()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/{custID}", handleParameters)
+
+	err := http.ListenAndServe("localhost:8080", r)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+}
 
-	defer db.CloseConnection()
+func handleParameters(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	fmt.Println(pathParams)
+
+	limit := r.URL.Query().Get("limit")
+	offset := r.URL.Query().Get("offset")
+
+	fmt.Printf("limit: %s\n", limit)
+	fmt.Printf("offset: %s\n", offset)
 }
